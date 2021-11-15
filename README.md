77a2f2509fdfbfaf985bbdf608224637c0f4ac2c
adeola <durodolaadeola7@gmail.com>
2021-11-15 17:38:44 +0100

    if statement

---
+++ README.md
@@ -0,0 +1,25 @@
+# SITE NAME #
+## online voting ##
+# NAME OF CONTRIBUTOR #
+## DURODOLA ADEOLA ##
+# BRIEF DESCRIPTION OF SITE #
+### this is an online voting site whereby you can vote and be voted for online
+## TECHNOLOGY USED 
+* VSCODE
+* HTML
+* CSS
+* GIT
+* bootstrap
+
+# HOW TO VISIT
+[click] {durodolaadeola.github.io/account}
+
+
+# KNOWN BUGS
+* using the if statement
+
+# contact #
+ * DURODOLA ADEOLA
+ * 08032847264
+ ### GITHUB ACCOUNT
+ [DURODOLA ADEOLA]
--- js/script.js
+++ js/script.js
@@ -1,5 +1,40 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//userinterface
 $(document).ready(function(){
+    
+   
     $(".val").click(function(){
         $(".form").toggle();
-    })
+      })
+      $(".form").submit(function(event){
+          event.preventDefault();
+        let age = parseInt($("#age").val());
+        if(age > 18){
+            window.location.href="register.html"
+        }
+        else if(age<18){
+            window.location.href="voting information.html"
+        }
+
+      })
+     
+   
+   
+    
+    
+   
+    
 })
